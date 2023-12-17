e21af00de8e3469ece56680f3d3f448a8844c48f
larinpaul <www.larinpavel@mail.ru>
2023-12-17 15:16:57 +0500

    Initial commit

---
+++ src/main/java/com/example/kotlinspringlaptopbab/KotlinspringlaptopbabApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotlinspringlaptopbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinspringlaptopbabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinspringlaptopbabApplication.class, args);
+	}
+
+}
