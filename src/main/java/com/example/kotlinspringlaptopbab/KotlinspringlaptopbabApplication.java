7d1ab7fdcc4cf8aced441312b0ecc5669d232cb1
larinpaul <www.larinpavel@mail.ru>
2023-12-17 16:20:12 +0500

    Added JPA @Entity

--- src/main/java/com/example/kotlinspringlaptopbab/KotlinspringlaptopbabApplication.java
+++ src/main/java/com/example/kotlinspringlaptopbab/KotlinspringlaptopbabApplication.java
@@ -2,12 +2,17 @@ package com.example.kotlinspringlaptopbab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class KotlinspringlaptopbabApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hey there! :)");
 		SpringApplication.run(KotlinspringlaptopbabApplication.class, args);
 	}
+
 
 }
